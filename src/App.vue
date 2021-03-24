296f4ae2bbd9f411ca166217810349eab1424b74
Cristian Ribeiro Etchebest <65380172+etchebest@users.noreply.github.com>
2021-03-24 16:39:43 -0300

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,78 @@
+<template>
+  <div id="app">
+    <Modal ref="myModal">
+      <template slot="header">
+        <h1>Modal</h1>
+      </template>
+      <template slot="content">
+        <p>
+          Você está prestes a marcar como concluída a AVD, deseja continuar?
+        </p>
+      </template>
+      <template slot="footer">
+        <button @click="confirmar()">Confirmar</button>
+      </template>
+    </Modal>
+    <button @click="show()">Open modal</button>
+  </div>
+</template>
+
+<script>
+import Modal from "./components/Modal";
+
+export default {
+  name: "App",
+
+  components: {
+    Modal,
+  },
+  methods: {
+    show() {
+      this.$refs.myModal.abrirModal();
+    },
+    confirmar() {
+      this.$refs.myModal.fecharModal();
+      alert("Confirmado!");
+    },
+  },
+};
+</script>
+
+<style lang="scss">
+#app {
+}
+</style>
+
+
+
+<!--
+// Props
+
+// <modal>
+//   <template slot="header">
+//     conteudo
+//   </template>
+//   <template slot="content">
+//     conteudo
+//   </template>
+//   <template slot="fotter">
+//     botao
+//   </template>
+// </modal>
+
+
+
+Modal
+<div>
+  <slot name="header">
+    conteudo template
+  </slot>
+</div> -->
+
+
+// Slots
+
+// - Modal -
+// Header
+// Conteudo
+// Footer(Botões)
---
+++ src/components/Modal.vue
@@ -0,0 +1,59 @@
+<template>
+  <div class="content" v-show="cModal">
+    <div class="modal">
+      <button @click="fecharModal">Fechar</button>
+      <slot name="header"> </slot>
+      <slot name="content"> </slot>
+      <slot name="footer"> </slot>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Modal",
+
+  data() {
+    return {
+      modal: false,
+    };
+  },
+  props: {
+    show: Boolean,
+  },
+  computed: {
+    cModal() {
+      return this.modal;
+    },
+  },
+  methods: {
+    abrirModal() {
+      this.modal = true;
+    },
+    fecharModal() {
+      this.modal = false;
+    },
+  },
+};
+</script>
+
+<style lang="scss" >
+.content {
+  position: absolute;
+  display: flex;
+  justify-content: center;
+  background-color: rgba(0, 0, 0, 0.1);
+  width: 100vw;
+  height: 100vh;
+  .modal {
+    display: flex;
+    flex-flow: column nowrap;
+    align-items: center;
+    background-color: #ccc;
+    width: 80vh;
+    margin: auto;
+    border-radius: 10px;
+    z-index: 99999;
+  }
+}
+</style>
